8fe9a890d80160351ac57927028214bf377f9f86
Wesley Keetch <keetchcode@gmail.com>
2024-10-02 10:08:49 -0600

    Submitting AboutMe assignment

---
+++ AboutMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Wesley Keetch on 9/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
